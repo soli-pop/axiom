e84e3557f8f6c1cb6e0d541ed09161f35ba3b52c
Solix <familyuse2241@gmail.com>
2026-04-11 20:07:47 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-createRoot(document.getElementById('root')).render(<StrictMode><App/></StrictMode>)
+import SupabaseTest from './SupabaseTest'
+// ...
+root.render(<SupabaseTest />)
