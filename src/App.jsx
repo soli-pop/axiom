e707c1c8f7771e019b833a05a95d702f70ac07d9
Solix <familyuse2241@gmail.com>
2026-04-11 19:38:45 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1839,3 +1839,61 @@ export default function App() {
     </div>
   );
 }
+
+import { useState } from "react"
+import { supabase } from "./lib/supabase"
+
+export default function App() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+
+  const testInsert = async () => {
+    const { data, error } = await supabase
+      .from("users_data")
+      .insert([
+        {
+          name,
+          email,
+          message
+        }
+      ])
+
+    console.log("DATA:", data)
+    console.log("ERROR:", error)
+
+    if (error) {
+      alert("Insert failed")
+    } else {
+      alert("Saved to Supabase!")
+    }
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Supabase Test Form</h2>
+
+      <input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+
+      <input
+        placeholder="Message"
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <br />
+
+      <button onClick={testInsert}>
+        Send to Supabase
+      </button>
+    </div>
+  )
+}
